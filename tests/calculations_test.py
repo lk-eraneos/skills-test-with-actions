9edec040e7588fc1ac90d14b9a31c3fc41a8f24f
lk-eraneos <lotte.klomp@eraneos.com>
2025-09-01 12:09:25 +0000

    Error fixes

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -1,6 +1,7 @@
 # System Modules
 import sys
 import os
+import unittest
 
 # Installed Modules
 # None
@@ -69,20 +70,20 @@ def test_get_nth_fibonacci_ten():
     # Assert
     assert result == 55
 
-def test_area_of_circle_negative_radius(self):
+def test_area_of_circle_negative_radius():
    """Test with a negative radius to raise ValueError."""
    # Arrange
    radius = -1
 
    # Act & Assert
-   with self.assertRaises(ValueError):
-      area_of_circle(radius)
+   with unittest.TestCase().assertRaises(ValueError):
+        area_of_circle(radius)
 
-def test_get_nth_fibonacci_negative(self):
+def test_get_nth_fibonacci_negative():
    """Test with a negative number to raise ValueError."""
    # Arrange
    n = -1
 
    # Act & Assert
-   with self.assertRaises(ValueError):
+   with unittest.TestCase().assertRaises(ValueError):
       get_nth_fibonacci(n)
